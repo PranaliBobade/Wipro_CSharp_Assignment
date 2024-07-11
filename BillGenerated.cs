e7e4049c8d4d84e3c88e38c93cc2d8b5fbb1687c
PranaliBobade <130236069+PranaliBobade@users.noreply.github.com>
2024-07-11 22:07:16 +0530

    Add files via upload

---
+++ BillGenerated.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace billgenerate
+{
+    class BillGenerated
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numner Of Pizza");
+            int numOfPizza = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number Of Puffs");
+            int numOfPuffs = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number Of Pepsis");
+            int numOfPepsis= int.Parse(Console.ReadLine());
+
+
+            double pizzaOfcost = numOfPizza * 300;
+            double puffsOfcost = numOfPuffs * 50;
+            double pepsisOfcost = numOfPepsis * 130;
+            
+
+            double totalOfCost = pizzaOfcost + puffsOfcost + pepsisOfcost;
+            double gstAmount = 0.09 * totalOfCost;
+                double cessAmount = 0.09 * totalOfCost;
+            double grandTotal = totalOfCost + gstAmount + cessAmount;
+            Console.WriteLine("\n Bill Details");
+            Console.WriteLine("Total cost of pizzas"+pizzaOfcost);
+            Console.WriteLine("Total cost of puffs"+puffsOfcost);
+            Console.WriteLine("Total cost of pepsis"+pepsisOfcost);
+            Console.WriteLine("Gst"+gstAmount);
+            Console.WriteLine("Cess"+cessAmount);
+            Console.WriteLine("Total Amount" + grandTotal);
+        }
+    }
+}
---
+++ PaySlip.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace PaySlip
+{
+    class paySlipGenerated
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee ID");
+            int empId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name");
+            string empName = Console.ReadLine();
+
+            Console.WriteLine("Enter Basic Salary");
+           double basicSalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Are You contract employee?(true/false):");
+            bool isContract= bool.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Department Number");
+            int depNo= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email ID");
+            string emailId = Console.ReadLine();
+
+            double hra = 0.15 * basicSalary;
+            double ma = 0.10 * basicSalary;
+            double ta = 0.15 * basicSalary;
+
+            double netSalary = basicSalary + hra + ma + ta;
+            Console.WriteLine("\n Enter Employee Details");
+            Console.WriteLine("Enter Employee ID"+empId);
+            Console.WriteLine("Enter Employee Name"+empName);
+            Console.WriteLine("Enter Basic Salary"+basicSalary);
+            Console.WriteLine("HRA" + hra);
+            Console.WriteLine("Ma" + ma);
+            Console.WriteLine("Ta" + ta);
+            Console.WriteLine("Total Salary" + netSalary);
+
+            Console.WriteLine("Are You contract employee?(true/false):"+isContract);
+            Console.WriteLine("Enter Department Number"+depNo);
+            Console.WriteLine("Enter Email ID"+emailId);
+        }
+    }
+}
+
